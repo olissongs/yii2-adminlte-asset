33899ff81e5361980bda24742bc7b11140d36b0d
olissongs <info@osvision.de>
2023-02-05 10:27:21 +0100

    Update left.php

--- example-views/yiisoft/yii2-app/layouts/left.php
+++ example-views/yiisoft/yii2-app/layouts/left.php
@@ -9,7 +9,7 @@
     <div class="sidebar">
 
         <nav class="mt-2">
-            <?= dmstr\adminlte\widgets\Menu::widget(
+            <?= olissongs\adminlte\widgets\Menu::widget(
                 [
                     'options' => ['class' => 'nav nav-pills nav-sidebar flex-column', 'data-widget' => 'treeview'],
                     'items' => [
